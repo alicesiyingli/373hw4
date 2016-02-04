ae7d6e157490927808b54020a1a4fb36002c0c1a
Siying Li <siyingli@dc3swp76.dc.umich.edu>
2016-02-03 19:22:57 -0500

    aa

---
+++ reverse_sort.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "sort.h"
+
+int compare(int a, int b) {
+	    return b - a;
+}
+
+int cmp (const void *a,const void *b){
+return(*(int*)b-*(int*)a);
+}
+
